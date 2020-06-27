19b65f4ef084425b48fa718171953ac66f448f29
Theux17 <oliveira.ms18@gmail.com>
2020-06-27 02:23:48 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const TelegramBot = require("node-telegram-bot-api")
+const token = "1065926966:AAG1BhA9ZSuKDn2BWIrsHOhuNnIczyXXJ0Q"
+
+const bot = new TelegramBot(token, {polling: true})
+
+const { Random } = require('random-js')
+const random = new Random
+
+
+bot.on('new_chat_members', msg => {
+    bot.sendMessage(
+        msg.chat.id, 
+        `Olá ${msg.from.first_name}, bem vindo ao chat Positivo!! Desabafe conosco, somos amigos!` 
+    )
+})
+
+bot.on("message", msg => {
+    const chatdId = msg.chat.id
+    let randomMessage = positivity[random.integer(0, positivity.length - 1)]
+    bot.sendMessage(chatdId, randomMessage)
+})
+
+const positivity = [
+    'Foque nos objetivos',
+    'Você vai alcançar',
+    'Basta ter fé!',
+    'Foco sempre!'
+]
